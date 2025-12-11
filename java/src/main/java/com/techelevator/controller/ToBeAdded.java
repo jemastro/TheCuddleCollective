f6c8a4a8e9b264e60737c77a9a52f04ef5a19675
Max Logue <maxlogue64@gmail.com>
2025-12-11 10:58:33 -0500

    Possible React Code and Steps to display multiple images per pet.  DO NOT push with this class still existing

--- java/src/main/java/com/techelevator/controller/ToBeAdded.java
+++ java/src/main/java/com/techelevator/controller/ToBeAdded.java
@@ -26,7 +26,39 @@ public class ToBeAdded
 //        return adoptedPets;
 //    }
 
+    // Add to a side nav?  Will eventually need to shift the grid to make it fit somehow.  Talk to Sarah
+    // about the design choices. Limit the adopted pets shown to the last 3 pets? Food for thought.
+    // Vertical side nav saying (Our Recent Fur-ever Home Finders) with the animal cards.
+
+
     // Multiple Photos Per Pet
+    // This might change later depending on if we use the PetCard in React that Sarah built.
+    // Assuming we use the same card then the steps are as follows:
+    // 1) Display the PetCard normally as it is currently built.
+    // 2) Check to see if there is more than one image attached to the Pet (img_url1)
+    // 3) If there are, display the first and second images, otherwise just the one.
+    // 4) If there's two images,Check to see if there are more than two images attached to the Pet (img_url2).
+    // 5) If there are, display all images, otherwise just the two.
 
+/*
+React code for displaying multiple images
+import React from 'react';
 
+const ProfileGallery = ({ images }) => {
+  return (
+    <div className="profile-images-container">
+      {images.map((imageUrl, index) => (
+        <img
+          key={index} // A unique ID is better, but index works if no stable ID exists
+          src={imageUrl}
+          alt={`Profile image ${index + 1}`}
+          className="profile-thumbnail"
+        />
+      ))}
+    </div>
+  );
+};
+
+export default ProfileGallery;
+ */
 }
