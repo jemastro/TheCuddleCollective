28ff19eddc46a9ba3419f82b0e908f31c621cd4a
Sarah Borgelt <borgelt.sarah@gmail.com>
2025-12-11 15:44:52 -0500

    Adding logo

--- react/src/components/Header/Header.jsx
+++ react/src/components/Header/Header.jsx
@@ -1,16 +1,15 @@
-import Placeholder2 from '../../assets/Placeholder2.jpg';
+import Logo from '../../assets/logo.png';
 import styles from './Header.module.css';
 
 export default function Header() {
      return (
      <div className={styles.banner}>
           <img 
-               src={Placeholder2} 
+               src={Logo} 
                alt="Collective Cuddle Logo" 
                className={styles.bannerImage} 
           />
      <div className={styles.bannerText}>
-          <p className={styles.italicize}>Finding pets homes, fur good!</p>
      </div>
      </div>
      );
