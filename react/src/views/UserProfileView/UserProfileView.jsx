276f623f21a0a8189b14e939a5f632a3875af56b
Sarah Borgelt <borgelt.sarah@gmail.com>
2025-12-12 10:44:27 -0500

    Update is functionalgit add .git add .git add .

--- react/src/views/UserProfileView/UserProfileView.jsx
+++ react/src/views/UserProfileView/UserProfileView.jsx
@@ -1,14 +1,15 @@
 import { useContext } from 'react';
 import { UserContext } from '../../context/UserContext';
+import styles from './UserProfileView.module.css';
 
 export default function UserProfileView() {
   const { user } = useContext(UserContext);
 
   return (
-    <div>
+    <div className={styles.userProfile}>
       <h1>User Profile</h1>
       <br />
-      <p>Hello, {user.username}!</p>
+      <p className={styles.profileGreeting}>Hello, {user.username}!</p>
     </div>
   );
 }
