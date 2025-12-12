c62690e2e99417d44a05bef0a2930fc1d5e5c6eb
Max Logue <maxlogue64@gmail.com>
2025-12-12 14:15:55 -0500

    Carousel progress???

--- react/src/components/AdoptedPetCarousel/AdoptedPetCarousel.jsx
+++ react/src/components/AdoptedPetCarousel/AdoptedPetCarousel.jsx
@@ -1,5 +1,7 @@
-// import React from "react";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 import Slider from "react-slick";
+
 
 export default function SimpleSlider() {
   var settings = {
--- react/src/views/HomeView/HomeView.jsx
+++ react/src/views/HomeView/HomeView.jsx
@@ -1,9 +1,12 @@
+import AdoptedPetService from '../../services/AdoptedPetService';
+import AdoptedPetCarousel from '../../components/AdoptedPetCarousel/AdoptedPetCarousel'
 import styles from './HomeView.module.css';
 
 export default function HomeView() {
 
   return (
     <div className={styles.container}>
+      {/* <span><AdoptedPetCarousel /></span> */}
       <h1 className={styles.welcome}>Welcome to the Cuddle Collective</h1>
       <br />
       <p className={styles.homePageText}>The Cuddle Collective is a warm, community-driven space dedicated to helping every animal 
@@ -11,7 +14,8 @@ export default function HomeView() {
         and connect with shelters in their area. We also make it easy for animal lovers to get involved 
         — from signing up to volunteer at local shelters to supporting rescue efforts across the community. 
         Together, we’re creating a world where every pet is welcomed, cared for, and truly home — 
-        fur good.</p>
+        fur good.</p>  
     </div>
+  
   );
 }
