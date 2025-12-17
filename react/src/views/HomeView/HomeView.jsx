2b97efd605b9bb29a98ccf16a1d09f4ab4689e50
Sarah Borgelt <borgelt.sarah@gmail.com>
2025-12-17 14:59:18 -0500

    Adding new background image and updated user profile

--- react/src/views/HomeView/HomeView.jsx
+++ react/src/views/HomeView/HomeView.jsx
@@ -1,7 +1,6 @@
 // import AdoptedPetService from '../../services/AdoptedPetService';
 // import AdoptedPetCarousel from '../../components/AdoptedPetCarousel/AdoptedPetCarousel'
 import styles from './HomeView.module.css';
-import animalsImg from '../../assets/AnimalsForAdoption.png';
 import { useNavigate } from 'react-router-dom';
 import FamilyWithPets from '../../assets/FamilyWithPets.jpg'
 
@@ -10,22 +9,22 @@ export default function HomeView() {
 
   return (
     <div className={styles.container}>
-      <img src={FamilyWithPets} className={styles.FamilyWithPets}/>
-      <h1 className={styles.welcome}>Welcome to the Cuddle Collective</h1>
-      {/* <br />
-      <h2>Recently Adopted Pets:
-        <AdoptedPetCarousel />
-      </h2>
-      <br /> */}
-      <p className={styles.homePageText}>The Cuddle Collective is a warm, community-driven space dedicated to helping every animal 
-        find a loving home. Here, future pet parents can browse adoptable pets, learn their stories, 
-        and connect with shelters in their area. We also make it easy for animal lovers to get involved 
-        — from signing up to volunteer at local shelters to supporting rescue efforts across the community. 
-        Together, we’re creating a world where every pet is welcomed, cared for, and truly home — 
-        fur good.</p>
-      <img className={styles.homePageImage}src={animalsImg} alt="A group of pets waiting for their new owner"/>
-      <button className={styles.callToAction} onClick={() => navigate('/availablePets')}>Are you ready to help us find our fur-ever home?</button>
+      <div className={styles.text}>
+        <h1 className={styles.welcome}>Welcome to the Cuddle Collective</h1>
+        {/* <br />
+        <h2>Recently Adopted Pets:
+          <AdoptedPetCarousel />
+        </h2>
+        <br /> */}
+        <p className={styles.homePageText}>The Cuddle Collective is a warm, community-driven space dedicated to helping every animal 
+          find a loving home. Here, future pet parents can browse adoptable pets, learn their stories, 
+          and connect with shelters in their area. We also make it easy for animal lovers to get involved 
+          — from signing up to volunteer at local shelters to supporting rescue efforts across the community. 
+          Together, we’re creating a world where every pet is welcomed, cared for, and truly home — 
+          fur good.</p>
+        <button className={styles.callToAction} onClick={() => navigate('/availablePets')}>Are you ready to help us find our fur-ever home?</button>
+      </div>
     </div>
-  
+    
   );
 }
--- react/src/views/UserProfileView/UserProfileView.jsx
+++ react/src/views/UserProfileView/UserProfileView.jsx
@@ -4,12 +4,16 @@ import styles from './UserProfileView.module.css';
 
 export default function UserProfileView() {
   const { user } = useContext(UserContext);
+  const isAdmin = user?.authorities?.some(auth => auth.name === "ROLE_admin");
 
   return (
     <div className={styles.userProfile}>
       <h1>User Profile</h1>
       <br />
       <p className={styles.profileGreeting}>Hello, {user.username}!</p>
+            {isAdmin ? (
+              <p className={styles.accessLevel}> Role: Admin</p>
+            ) : <p className={styles.accessLevel}>Role: Volunteer</p>} 
     </div>
   );
 }
