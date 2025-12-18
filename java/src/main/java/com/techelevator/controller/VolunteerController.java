42f5bc41d67ec7284aa14965a1b8c45a7a86a51c
Jenna Mastrodonato <jemastro@outlook.com>
2025-12-18 11:12:47 -0500

    partial edit

--- java/src/main/java/com/techelevator/controller/VolunteerController.java
+++ java/src/main/java/com/techelevator/controller/VolunteerController.java
@@ -10,7 +10,7 @@ import java.util.List;
 
 @RestController
 @CrossOrigin
-@PreAuthorize("isAuthenticated()")
+@PreAuthorize("hasAnyRole('ADMIN', 'VOLUNTEER')")
 public class VolunteerController {
 
     private final VolunteerDao volunteerDao;
@@ -21,24 +21,24 @@ public class VolunteerController {
     }
 
     @GetMapping("/volunteer/directory")
-    @PreAuthorize("isAuthenticated()")
+    @PreAuthorize("hasAnyRole('ADMIN', 'VOLUNTEER')")
     public List<ShelterVolunteer> listAllVolunteers(){
         return volunteerDao.getAllVolunteers();
     }
 
-    @PreAuthorize("isAuthenticated()")
+    @PreAuthorize("hasAnyRole('ADMIN', 'VOLUNTEER')")
     @PostMapping("/volunteers")
     public ShelterVolunteer addVolunteer(@RequestBody ShelterVolunteer v) {
         return volunteerDao.createVolunteer(v);
     }
 
-    @PreAuthorize("isAuthenticated()")
+    @PreAuthorize("hasAnyRole('ADMIN', 'VOLUNTEER')")
     @DeleteMapping("/volunteers/{id}")
     public void deleteVolunteer(@PathVariable int id) {
         volunteerDao.deleteVolunteer(id);
     }
 
-    @PreAuthorize("isAuthenticated()")
+    @PreAuthorize("hasAnyRole('ADMIN', 'VOLUNTEER')")
     @PutMapping("/volunteers/{id}")
     public ShelterVolunteer updateVolunteer(@PathVariable String id, @RequestBody ShelterVolunteer volunteer) {
         volunteer.setVolunteerId(id);
